44459dc82d556c698693cc63c2be2dd65144f4df
Gael Enrique Guzman Iribe <141987850+A01742941Gael@users.noreply.github.com>
2023-08-14 18:35:47 -0700

    Add files via upload

---
+++ exercise (1).py
@@ -0,0 +1,12 @@
+def main():
+    #escribe tu código abajo de esta línea
+    pass
+edad1=int(input("Año de nacimiento: "));
+edad2=int(input("Año actual: "));
+Lustros=(edad2-edad1)/5
+print("lustros: ",Lustros)
+    
+
+
+if __name__ == '__main__':
+    main()
---
+++ exercise.py
@@ -0,0 +1,13 @@
+def main():
+    #escribe tu código abajo de esta línea
+    pass
+num1=int(input("Dame tu edad: "))
+num2=int(input("Dame el año: "))
+Suma=(num2-num1)+100
+print("Tendras 100 años en: ",Suma)
+
+
+
+
+if __name__ == '__main__':
+    main()
